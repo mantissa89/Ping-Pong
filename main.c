240a0e1756633d568a4bd65a8ee9b45926a5d65c
mantissa89 <smithlee06@outlook.com>
2026-06-19 00:04:16 +0100

    Implement game loop
    Signed-off-by: mantissa89 <smithlee06@outlook.com>

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include "raylib.h"
 
+
 int main(){
-    
+    int screenWidth = 800;
+    int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Ping Pong");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;  
 }
